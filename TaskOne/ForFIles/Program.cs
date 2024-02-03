a46e6d16a3a7633a4873dbabdb9cff1c52ce0432
Roronoa1331 <potterhappy55@gmail.com>
2024-02-04 00:53:07 +0400

    salam

---
+++ TaskOne/ForFIles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ForFIles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string[] sourceFilePath =
+            {
+                "C:\\Users\\potte\\OneDrive\\Masaüstü\\doing tasks\\name1.txt",
+                "C:\\Users\\potte\\OneDrive\\Masaüstü\\doing tasks\\name2.txt",
+                "C:\\Users\\potte\\OneDrive\\Masaüstü\\doing tasks\\name3.txt"
+
+            };
+            string folderPath = @"C:\Users\potte\OneDrive\Masaüstü\doing tasks\";
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+            string destinationFilePath = @"C:\Users\potte\OneDrive\Masaüstü\doing tasks\name4.txt";
+
+            FileInfo newfile = new FileInfo(destinationFilePath);
+            
+
+            string combiningContent = " ";
+            foreach (string item in sourceFilePath)
+            {
+                StreamReader reader = new StreamReader(item);
+                combiningContent += reader.ReadToEnd() + Environment.NewLine;
+                reader.Close();
+            }
+
+            StreamWriter writer = new StreamWriter(destinationFilePath);
+            writer.Write(combiningContent);
+            writer.Close();
+
+
+        }
+    }
+}
