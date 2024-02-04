c189ba4eba52d3c9f8b602a5004fe0f756b146ca
Roronoa1331 <potterhappy55@gmail.com>
2024-02-04 17:08:42 +0400

    second commit

---
+++ TaskTwo/Tasks2ForFiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks2ForFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 2 
+            string fileName = Console.ReadLine();
+            string folderPath = "C:\\Users\\potte\\OneDrive\\Masaüstü\\task2\\";
+            string folder = Console.ReadLine();
+            string finalPath = Path.Combine(folderPath, folder);
+            DirectoryInfo directoryInfo = new DirectoryInfo(finalPath);
+           
+
+            if(!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+            directoryInfo.Refresh();
+            DateTime creationDate = directoryInfo.CreationTime;
+
+
+            Console.WriteLine(folder + " Created");
+            Console.WriteLine(creationDate + " - it had been created this date");
+            FileInfo file = new FileInfo(@"C:\\Users\\potte\\OneDrive\\Masaüstü\\task2\\" + folder + "\\" +fileName);
+            StreamWriter str = file.CreateText();
+
+        }
+    }
+}
